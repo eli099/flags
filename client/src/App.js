3ad4d9c7d8d6d10c447f68c97d6d70f9a7cb037e
Eliott McKenzie <e4991m@gmail.com>
2023-08-22 20:11:15 +0100

    digging down into json object

--- client/src/App.js
+++ client/src/App.js
@@ -8,7 +8,7 @@ const App = () => {
   useEffect(() => {
     const getData = async () => {
       try {
-        const { data } = await axios.get('https://restcountries.com/v2/all')
+        const { data } = await axios.get('https://restcountries.com/v3.1/all')
         console.log('countries ->', countries)
         setCountries(data)
       } catch (error) {
@@ -22,6 +22,17 @@ const App = () => {
     <div className="container">
       {/* Filters */}
       {/* Countries */}
+      <div className="country-container">
+        {countries.map((country, index) => {
+          const { ccn3, name, flags } = country
+          console.log('ccn3 ->', ccn3, 'name ->', name, 'name.nativeName ->', name.nativeName, 'flags.png ->', flags.png)
+          return (
+            <div className="country" key={index}>
+
+            </div>
+          )
+        })}
+      </div>
     </div>
   )
 }
